bb4b3b24a8fcba1c38818901073b796692a3efb9
shahroze90 <shahrozetariq@hotmail.com>
2016-04-03 14:50:46 +0000

    removed styles

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,31 +14,3 @@
 // = require jquery_ujs
 // = require turbolinks
 // = require_tree .
-
-/*global*/
-(function() {
-	var container	= ('#ib-container'),
-	articles	= container.children('article'),
-	timeout;
-	articles.on( 'mouseenter', function( event ) {
-		var article	= (this);
-		clearTimeout( timeout );
-		timeout = setTimeout( function() {			
-			if( article.hasClass('active') ) return false;			
-			articles.not( article.removeClass('blur').addClass('active') )
-			.removeClass('active')
-			.addClass('blur');			
-		}, 65 );		
-	});	
-	container.on( 'mouseleave', function( event ) {		
-		clearTimeout( timeout );
-		articles.removeClass('active blur');		
-	});
-});
-
-(function() {
-	('a.nice-link').each(function () {
-	  (this).attr('data-text', (this).text());
-	});
-});
-
