91215f74b9345b760d70291dfc2451865166dede
shahroze90 <shahrozetariq@hotmail.com>
2016-04-03 14:36:07 +0000

    heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,30 +15,30 @@
 // = require turbolinks
 // = require_tree .
 
-
-$(function() {
-	var $container	= $('#ib-container'),
-	$articles	= $container.children('article'),
+/*global*/
+(function() {
+	var container	= ('#ib-container'),
+	articles	= container.children('article'),
 	timeout;
-	$articles.on( 'mouseenter', function( event ) {
-		var $article	= $(this);
+	articles.on( 'mouseenter', function( event ) {
+		var article	= (this);
 		clearTimeout( timeout );
 		timeout = setTimeout( function() {			
-			if( $article.hasClass('active') ) return false;			
-			$articles.not( $article.removeClass('blur').addClass('active') )
+			if( article.hasClass('active') ) return false;			
+			articles.not( article.removeClass('blur').addClass('active') )
 			.removeClass('active')
 			.addClass('blur');			
 		}, 65 );		
 	});	
-	$container.on( 'mouseleave', function( event ) {		
+	container.on( 'mouseleave', function( event ) {		
 		clearTimeout( timeout );
-		$articles.removeClass('active blur');		
+		articles.removeClass('active blur');		
 	});
 });
 
-$(function() {
-	$('a.nice-link').each(function () {
-	  $(this).attr('data-text', $(this).text());
+(function() {
+	('a.nice-link').each(function () {
+	  (this).attr('data-text', (this).text());
 	});
 });
 
